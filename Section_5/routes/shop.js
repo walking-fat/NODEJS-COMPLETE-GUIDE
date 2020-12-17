7b36309fb5e3ec6ccc8b26090a92f5655f12bd19
Zeng Chen <chenzeng0112@gmail.com>
2020-12-17 00:39:12 -0600

    Section_5 & Assignment_3

--- Section_5/routes/shop.js
+++ Section_5/routes/shop.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path.js');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>HELLO from Express</h1>');
+// res.send('<h1>HELLO from Express</h1>');
+    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 
-module.exports = router;
+module.exports = router; 
